dfd5beaad1679a2f3d9ce466ad74c98734fb2a36
rubylarsen <rlarsen@lsoc.org>
2025-09-04 17:35:09 +0000

    wrote 54b code

---
+++ src/org/lsoc/HW54b.java
@@ -0,0 +1,14 @@
+public class HW54b{
+  public static void main(String[] args) {
+    int x = 475;
+    int y = 821;
+    int z = 369;
+    int a = 562;
+    int sum = x + y + z + a;
+    double avg = sum / 4.0 ;
+    System.out.println("The sum of the four numbers is " + sum);
+    System.out.println("The average of the four numbers is " + avg);
+
+  }
+
+}
