a805df96393fbe402aaf7af3e1b77a83c28b3ac2
rubylarsen <rlarsen@lsoc.org>
2025-09-04 17:22:09 +0000

    wrote 54c code

---
+++ src/org/lsoc/HW54c.java
@@ -0,0 +1,11 @@
+public class HW54c {
+  public static void main(String[] args){
+    double pi = 3.14159;
+    double radius = 3.712;
+    double area = radius * pi * radius;
+    double circ = 2 * pi * radius;
+    System.out.println(area);
+    System.out.println(circ);
+
+  }
+}
